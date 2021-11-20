b50dabecb1914bbf7917d52c95c4aa4b24b15279
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-20 16:45:06 +0530

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+    return(
+      <div className="home">
+              <h1>Home</h1>
+      </div>
+    )
 }
-
 export default App;
+
+/*
+ * class component
+ * 
+ * it is easy to accomodate
+ * 
+ * data [properties + states]
+ * template [html]
+ * Event Handling functions
+ * Css can be referred from an external file 
+ *
+ * 
+ * In a functional components usually we dont use state & event handling functions
+ * */
