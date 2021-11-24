029b041909a87f7359859f542dbfcb72b874a51f
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-24 15:04:12 +0530

    controlled-form

--- src/People.tsx
+++ src/People.tsx
@@ -14,16 +14,24 @@ const items:Person[]=[
 ]
 */
 const People=()=>{
-    const person=useRef<HTMLInputElement>(null)
+//ref creation for form element
+    const person:any=useRef<HTMLInputElement>()
+
+    const [sno,setSno]=useState("")
+    const [name,setName]=useState("")
+    const [city,setCity]=useState("")
+
+    //function for reading ref
     function addPerson(){
      //let item=  person.current;
      let content=person.current?.value
      let result:any=content?.split(",")
      addPersonCall({sno:Number(result[0]),name:result[1],city:result[2]});
      person.current?.focus()
-     
-     //console.log(item)
+     person.current.value=""
     }
+
+    //function for get Rest call
     async function peopleRead(){
           try{
               let response =
@@ -35,6 +43,14 @@ const People=()=>{
           }
     }
 
+    async function controlledStore(){
+        addPersonCall({sno:Number(sno),name:name,city:city})
+        setSno("")
+        setName("")
+        setCity("")
+    }
+
+    //function for post Rest call
     async function addPersonCall(obj:Person){
         try{
           const response= await axios.post("http://localhost:8000/api/people",obj)
@@ -45,14 +61,41 @@ const People=()=>{
         }
 
     }
+    //hook that runs after first rendering
     useEffect(()=>{
          peopleRead()
     },[]
         
     )
+
+    useEffect(()=>{
+        console.log(sno,name,city)
+    })
         const [peopleData,setPeopleData] =useState([])
     return(
         <div>
+          <div className="alert alert-info">
+             <table>
+                 <tr> 
+                    <td>Sno</td>
+                    <td><input type="text" value={sno}
+                    onChange={(e)=>{setSno(e.target.value)}}/></td>
+                 </tr> 
+                 <tr> 
+                    <td>Name</td>
+                    <td><input type="text" value={name}
+                    onChange={(e)=>{setName(e.target.value)}}/></td>
+                 </tr> 
+                 <tr> 
+                    <td>City</td>
+                    <td><input type="text" value={city}
+                    onChange={(e)=>{setCity(e.target.value)}}/></td>
+                 </tr> 
+                 <tr> 
+                     <td><button className="btn btn-success" onClick={controlledStore}>Add Person</button></td>
+                 </tr>    
+            </table>  
+          </div>
         <div className="alert alert-info">
             Person Data <input type="text" ref={person}></input>
             <br/>
